5a39d66a284a3d5556ca5d4f4facbf7ccf47fed7
ne2la <madushan.20191085@iit.ac.lk>
2021-08-21 23:58:13 +0530

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hell0 React </h1>
+      <Router>
+        <NavBar/>
+        <Route path="/" component={Home} exact/>
+        <Route path="/about" component={About}/>
+      </Router>
     </div>
   );
 }
