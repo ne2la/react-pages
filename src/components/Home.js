f110dcd858a6d2d82141fec652eabb9c943daefc
ne2la <madushan.20191085@iit.ac.lk>
2021-08-22 00:07:17 +0530

    add bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 const Home = () => {
     return (
         <div>
             <h1> This is Home </h1>
+            <Button variant="primary"> This is Home bootstrap Button </Button>
+
         </div>
     )
 }
